3953ee24a5dbe02246f2fac06fd80a8a0b403769
Yuichi <42515966+kamblerohit123@users.noreply.github.com>
2023-02-02 20:30:50 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,55 +1,50 @@
 const form = document.querySelector('#searchForm');
-const res = document.querySelector('#tableResult');
-var upd; 
+const res = document.querySelector('#resTable');
+const cont = document.getElementById("allContaint");
+
 form.addEventListener('submit',(e)=>{
-
     e.preventDefault();
-    if(upd){
-        clearTimeout(upd);
-    }
-
     const ctype = form.elements.coinType.value;
-
+    cont.classList.add('mainClick');
+    cont.classList.remove('main');    
     fetchPrice(ctype);
 
 });
 
-
-const fetchPrice= async(ctype) =>{
+const fetchPrice = async(ctype) =>{
     const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=USD`);
-    console.log(r.data.coin.price);
-     const price = r.data.coin.price;
-     const volume  = r.data.coin.volume;
-     const change = r.data.coin.priceChange1d;
-     const base = r.data.coin.name;
-     const target = 'USD';
-
-
-     res.innerHTML =`<tr style="background-color:blue; color:white; font-weight:700">
-     <td>
-         Property
-     </td>
-     <td>Value</td>
- </tr>
- <tr>
-     <td>
-         ${base}
-     </td>
-     <td>${price} ${target}</td>
- </tr>
- <tr>
-     <td>
-         Volume
-     </td>
-     <td>${volume}</td>
- </tr>
- <tr>
-     <td>
-         Change
-     </td>
-     <td>${change}</td>
- </tr>`
-
-    upd = setTimeout(()=>fetchPrice(ctype),10000);
-
+    showPrice(r.data.coin);
 }
+
+
+const showPrice = (coinData)=>{
+    const price = coinData.price;
+    const vol = coinData.volume;
+    const change = coinData.priceChange1d;
+    const coin = coinData.name;
+    const curr = 'USD';
+    var col= "green";
+    if(change<0){
+        col = "red";
+    }
+    res.innerHTML = `<tr class="bg-primary" style="color: white;">
+    <td>
+        Property
+    </td>
+    <td>
+        Value
+    </td>
+</tr>
+<tr>
+    <td>${coin}</td>
+    <td style="color:${col};"><span style="font-size: 1.3em;">${price}</span> ${curr}</td>
+</tr>
+<tr>
+    <td>Volume (24hrs)</td>
+    <td>${vol}</td>
+</tr>
+<tr>
+    <td>Change (24hrs)</td>
+    <td style="color:${col};">${change} ${curr}</td>
+</tr>`;
+};
